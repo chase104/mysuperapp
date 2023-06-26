d4d7b4e83137e54c50b3bb58ae9da8d65d21d13e
Chase Van Halen <cvanhalen@perscholas.org>
2023-06-26 13:10:14 -0500

    made form stuff

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const masterbranch = "awesome"
 
 console.log("changed from the html branch")
+
+
+let myForm = document.querySelector("#form");
+
+
+myForm.addEventListener('submit', () => {
+    console.log(("sumitting"));
+})
