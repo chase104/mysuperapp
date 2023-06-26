293bfdbd045294e2cf5c9a3b6061d396107a3e56
Chase Van Halen <cvanhalen@perscholas.org>
2023-06-26 13:05:44 -0500

    added button logic

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const masterbranch = "awesome"
 
 console.log("changed from the html branch")
+
+
+let myButton = document.querySelector("#button");
+
+myButton.addEventListener('click', (e) => {
+    console.log("button is clicked");
+})
